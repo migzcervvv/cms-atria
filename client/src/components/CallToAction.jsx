6a5c079a232b991422d2b32f7168ae9c9b4a3d2a
migzcervvv <migzcerv007@gmail.com>
2024-04-11 10:35:27 +0800

    ctat

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -5,7 +5,7 @@ export default function CallToAction() {
     <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
       <div className="flex-1 justify-center flex flex-col">
         <h2 className="text-2xl">Want to learn more about JavaScript?</h2>
-        <p className="text-gray-600 my-2 dark:text-gray-400">
+        <p className="text-gray-800 my-2 dark:text-gray-400">
           Check out these resources with 100 JavaScript projects
         </p>
         <Button

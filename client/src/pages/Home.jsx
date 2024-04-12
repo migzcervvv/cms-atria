e1058aded12d404b120308852c27a202a3f650c4
migzcervvv <migzcerv007@gmail.com>
2024-04-12 13:19:25 +0800

    complete home page again

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,7 +21,7 @@ export default function Home() {
         <h1 className="text-3xl font-bold lg:text-6xl">
           Welcome to Atria News
         </h1>
-        <p className="text-gray-500 text-xs sm:text-sm">
+        <p className="text-gray-600 text-sm sm:text-md dark:text-gray-400">
           At Atria, we're more than just a learning and development hub for
           maritime professionals. We're dedicated to fostering holistic growth,
           nurturing individuals with a blend of hands-on experience,
@@ -39,12 +39,12 @@ export default function Home() {
       <div className="p-3 bg-amber-100 dark:bg-slate-700">
         <CallToAction />
       </div>
-      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
+      <div className="max-w-8xl mx-auto p-3 flex flex-col gap-8 py-7">
         {" "}
         {posts && posts.length > 0 && (
           <div className="flex flex-col gap-6">
             <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
-            <div className="flex flex-wrap gap-4">
+            <div className="flex flex-wrap gap-4 items-center justify-center">
               {posts.map((post) => (
                 <PostCard key={post._id} post={post} />
               ))}

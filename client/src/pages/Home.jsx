e3a5d663466995993f9282e4465211eb6ce70a62
migzcervvv <migzcerv007@gmail.com>
2024-04-12 11:29:45 +0800

    complete the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,63 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
+import { useEffect, useState } from "react";
+import PostCard from "../components/PostCard";
 
 export default function Home() {
-  return <div>Home</div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchposts = async () => {
+      const res = await fetch("/api/post/getposts");
+      const data = await res.json();
+      setPosts(data.posts);
+    };
+    fetchposts();
+  }, []);
+
+  return (
+    <div>
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
+        <h1 className="text-3xl font-bold lg:text-6xl">
+          Welcome to Atria News
+        </h1>
+        <p className="text-gray-500 text-xs sm:text-sm">
+          At Atria, we're more than just a learning and development hub for
+          maritime professionals. We're dedicated to fostering holistic growth,
+          nurturing individuals with a blend of hands-on experience,
+          intellectual knowledge, and heartfelt dedication. Stay ahead of the
+          curve and explore the latest news and updates from Atria, your partner
+          in maritime excellence.
+        </p>{" "}
+        <Link
+          to="/search"
+          className="text-xs sm:text-sm text-teal-500 font-bold hover:underline"
+        >
+          View all posts
+        </Link>
+      </div>
+      <div className="p-3 bg-amber-100 dark:bg-slate-700">
+        <CallToAction />
+      </div>
+      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
+        {" "}
+        {posts && posts.length > 0 && (
+          <div className="flex flex-col gap-6">
+            <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
+            <div className="flex flex-wrap gap-4">
+              {posts.map((post) => (
+                <PostCard key={post._id} post={post} />
+              ))}
+            </div>
+            <Link
+              to={"/search"}
+              className="text-lg text-teal-500 hoveer:underline text-center"
+            >
+              View all posts
+            </Link>
+          </div>
+        )}{" "}
+      </div>
+    </div>
+  );
 }

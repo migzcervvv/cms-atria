b0b49097abae7af23474b8cd2195df601bfe2c84
migzcervvv <migzcerv007@gmail.com>
2024-04-17 10:00:37 +0800

    Removed warning from update post functionality

--- client/src/pages/UpdatePost.jsx
+++ client/src/pages/UpdatePost.jsx
@@ -1,4 +1,11 @@
-import { Alert, Button, FileInput, Select, TextInput } from "flowbite-react";
+import {
+  Alert,
+  Button,
+  FileInput,
+  Label,
+  Select,
+  TextInput,
+} from "flowbite-react";
 import { useEffect, useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
@@ -18,7 +25,7 @@ export default function UpdatePost() {
   const [file, setFile] = useState(null);
   const [imageUploadProgress, setImageUploadProgress] = useState(null);
   const [imageUploadError, setImageUploadError] = useState(null);
-  const [formData, setFormData] = useState({});
+  const [formData, setFormData] = useState(null);
   const [publishError, setPublishError] = useState(null);
   const { postId } = useParams();
 
@@ -82,10 +89,9 @@ export default function UpdatePost() {
       console.log(error);
     }
   };
-  console.log(formData.userId, currentUser._id);
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!formData._id) {
+    if (!formData.title) {
       setPublishError("You are not allowed to edit this post!");
       return;
     }
@@ -117,84 +123,91 @@ export default function UpdatePost() {
   return (
     <div className="p-3 max-w-3xl mx-auto min-h-screen">
       <h1 className="text-center text-3xl my-7 font-semibold">Update a post</h1>
-      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-        <div className="flex flex-col gap-4 sm:flex-row justify-between">
-          <TextInput
-            type="text"
-            placeholder="Title"
-            required
-            id="title"
-            className="flex-1"
-            onChange={(e) =>
-              setFormData({ ...formData, title: e.target.value })
-            }
-            value={formData.title}
-          />
-          <Select
-            onChange={(e) =>
-              setFormData({ ...formData, category: e.target.value })
-            }
-            value={formData.category}
-          >
-            <option value="uncategorized">Select a category</option>
-            <option value="Seafarers">Seafarers</option>
-            <option value="Atria">Atria</option>
-            <option value="Avior">Avior</option>
-          </Select>
-        </div>
+      {formData !== null ? (
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-4 sm:flex-row justify-between">
+            <TextInput
+              type="text"
+              placeholder="Title"
+              required
+              id="title"
+              className="flex-1"
+              onChange={(e) =>
+                setFormData({ ...formData, title: e.target.value })
+              }
+              value={formData.title || ""}
+            />
+            <Select
+              onChange={(e) =>
+                setFormData({ ...formData, category: e.target.value })
+              }
+              value={formData.category || "uncategorized"}
+            >
+              <option value="uncategorized">Select a category</option>
+              <option value="Seafarers">Seafarers</option>
+              <option value="Atria">Atria</option>
+              <option value="Avior">Avior</option>
+            </Select>
+          </div>
 
-        <div className="flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3">
-          <FileInput
-            type="file"
-            accept="image/*"
-            onChange={(e) => setFile(e.target.files[0])}
+          <div className="flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3">
+            <FileInput
+              type="file"
+              accept="image/*"
+              value={file !== null ? file : ""}
+              onChange={(e) => setFile(e.target.files[0])}
+            />
+            <Button
+              type="button"
+              gradientDuoTone="purpleToBlue"
+              size="sm"
+              outline
+              onClick={handleUploadImage}
+              disabled={imageUploadProgress}
+            >
+              {imageUploadProgress ? (
+                <div className="w-16 h-16">
+                  <CircularProgressbar
+                    value={imageUploadProgress}
+                    text={`${imageUploadProgress || 0}%`}
+                  />
+                </div>
+              ) : (
+                "Upload Image"
+              )}
+            </Button>
+          </div>
+          {imageUploadError && (
+            <Alert color="failure">{imageUploadError}</Alert>
+          )}
+          {formData.image && (
+            <img
+              src={formData.image}
+              alt="upload"
+              className="w-full h-72 object-cover"
+            />
+          )}
+          {/*TEXT EDITOR, NEED TO CHANGE REACT QUILL TEXT EDITOR IF DOM HAS DEPRECATED*/}
+          <ReactQuill
+            theme="snow"
+            value={formData.content || ""}
+            placeholder="Write Something..."
+            className="h-72 mb-12"
+            required
+            onChange={(value) => setFormData({ ...formData, content: value })}
           />
-          <Button
-            type="button"
-            gradientDuoTone="purpleToBlue"
-            size="sm"
-            outline
-            onClick={handleUploadImage}
-            disabled={imageUploadProgress}
-          >
-            {imageUploadProgress ? (
-              <div className="w-16 h-16">
-                <CircularProgressbar
-                  value={imageUploadProgress}
-                  text={`${imageUploadProgress || 0}%`}
-                />
-              </div>
-            ) : (
-              "Upload Image"
-            )}
+          <Button type="submit" gradientDuoTone="purpleToPink">
+            Update Post
           </Button>
-        </div>
-        {imageUploadError && <Alert color="failure">{imageUploadError}</Alert>}
-        {formData.image && (
-          <img
-            src={formData.image}
-            alt="upload"
-            className="w-full h-72 object-cover"
-          />
-        )}
-        {/*TEXT EDITOR, NEED TO CHANGE REACT QUILL TEXT EDITOR IF DOM HAS DEPRECATED*/}
-        <ReactQuill
-          theme="snow"
-          value={formData.content}
-          placeholder="Write Something..."
-          className="h-72 mb-12"
-          required
-          onChange={(value) => setFormData({ ...formData, content: value })}
-        />
-        <Button type="submit" gradientDuoTone="purpleToPink">
-          Update Post
-        </Button>
-        {publishError && (
-          <Alert className="mt-5" color="failure">
-            {publishError}
-          </Alert>
-        )}
-      </form>
+          {publishError && (
+            <Alert className="mt-5" color="failure">
+              {publishError}
+            </Alert>
+          )}
+        </form>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
